eca230694e20568f204d49f9f27c6264ef927f9c
Oleksandr Hlushach <oleksandr.hlushach@ristars.io>
2025-01-27 15:59:05 +0200

    fix: change getting data from remote server

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -2,23 +2,19 @@ import axios, { AxiosError } from 'axios';
 import { IResponse } from '../components/interfaces/response.interface';
 import { IPokemon } from '../components/interfaces/pokemon.interface';
 
-export async function fetchPokemon(): Promise<IPokemon[]> {
+export async function fetchPokemon(offset = 0): Promise<IPokemon[]> {
   try {
-    const searchStr: string = localStorage.getItem('searchStr') || '';
+    const searchStr: string = (localStorage.getItem('searchStr') || '').trim();
 
-    const url: string = searchStr
-      ? `https://pokeapi.co/api/v2/pokemon?limit=1304`
-      : `https://pokeapi.co/api/v2/pokemon?limit=20`;
+    const url: string = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`;
 
     const { data } = await axios.get<IResponse>(url);
 
-    const results = data.results
-      .filter((pokemon) =>
-        searchStr
-          ? pokemon.name.toLowerCase().includes(searchStr.toLowerCase())
-          : true
-      )
-      .slice(0, 20);
+    const results = data.results.filter((pokemon) =>
+      searchStr
+        ? pokemon.name.toLowerCase().includes(searchStr.toLowerCase())
+        : true
+    );
 
     return results;
   } catch (e: unknown) {
